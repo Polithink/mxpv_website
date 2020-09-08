710eaa5381233de8ea1e517fc70e0982546c8f37
jcarlos <jcpablo10@gmail.com>
2020-09-08 00:02:58 -0500

    form

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 import '../sass/index.scss'
 import images from './images'
+import sendForm from './sendForm'
 
 const imgHeader = document.getElementById('imgHeader')
 const imgMockupInterior = document.getElementById('imgMockupInterior')
 
 imgHeader.src = images.header
 imgMockupInterior.src = images.mockup_interior
+
+const newsletterForm = document.getElementById('newsletterForm')
+    
+    
+    newsletterForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const formData = new FormData(newsletterForm)
+        sendForm(formData)
+        // console.log(response)
+    })
---
+++ src/js/sendForm.js
@@ -0,0 +1,32 @@
+import urls from './urls.json'
+import swal from 'sweetalert';
+
+const pdf = 'http://mexicoprevieneac.org/wp-content/uploads/2017/11/sobrevivientesviales_version64_listofinalpg.pdf'
+
+const prod = urls.apiUrlProd
+const dev = urls.apiUrlDev
+const apiUrl = process.env.NODE_ENV === 'development' ? dev : prod ;
+
+const sendForm = async params => {
+
+    await fetch(apiUrl, {
+        method: 'post',
+        body: params
+    }).then(response => response.json())
+    .then( data => {
+        console.log(data)
+        if (!data.success) {
+            swal("Atención", data.data, "warning");
+        } else {
+            swal("¡Gracias!", data.data, "success");
+            // window.location.href = "./gracias.html"
+            window.open(pdf);
+        }
+        return data
+    })
+    .catch (function(){
+        swal("Error", 'Algo salió mal', "error");
+    })
+    
+}
+export default sendForm
